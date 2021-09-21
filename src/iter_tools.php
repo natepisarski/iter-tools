97be4a3a24cd3b98572a2ad3375b3e3adcaa4d5b
Nathaniel Pisarski <nathanpisarski@gmail.com>
2021-09-20 20:55:34 -0400

    Add Roadmap; add iter_reduce and iter_count
    The roadmap is a document that is tracking all
    of Laravel Collection's public methods. It will
    get ticked off one-by-one as new functions are added.
    
    Realistically, it will probably fall out of sync with the
    code itself during development. But, the end goal is to have
    each of those functions.

--- src/iter_tools.php
+++ src/iter_tools.php
@@ -16,15 +16,66 @@ namespace IterTools;
  * - For functions which require a specific format, the requirements will be listed with #[ArrayShape] or a documentation comment.
  */
 
-if (!function_exists('IterTools\all')) {
-    function all(?iterable $iterable): iterable
+if (!function_exists('IterTools\iter_all')) {
+
+    /**
+     * Given any iterable, return it as an array.
+     * @param iterable|null $iterable Your iterable, or null.
+     * @return iterable The iterable as an array. Or, an empty array for 'null'.
+     */
+    function iter_all(?iterable $iterable): iterable
     {
         $finalArray = [];
 
-        foreach ($iterable as $item) {
+        foreach ($iterable ?? [] as $item) {
             array_push($finalArray, $item);
         }
 
         return $finalArray;
+    }
+}
+
+if (!function_exists('IterTools\iter_count')) {
+
+    /**
+     * Returns the overall number of items in this list.
+     * @param iterable|null $iterable Your iterable, or null
+     * @return int The number of items in the list. null items still count as items.
+     */
+    function iter_count(?iterable $iterable): int
+    {
+        $count = 0;
+        foreach ($iterable as $item) {
+            $count++;
+        }
+
+        return $count;
+    }
+}
+
+if (!function_exists('IterTools\iter_reduce')) {
+
+    /**
+     * Reduces an iterable to a single value.
+     * @param iterable|null $iterable Your iterable, or null
+     * @param callable $reducer Your function. This is a function of the form
+     *
+     *   ($carry, $item) => <NEW_CARRY> where $carry is the overall value so far, and $item is the current item in the list.
+     *
+     * The function can also take this form:
+     *
+     *   ($carry, $item, $key) => <NEW_CARRY>
+     *
+     * which is functionally the same, but the key of an associative array is passed in the $key variable.
+     * @param mixed|null $initialValue The initial value on the first step in reducing. Defaults to 'null'
+     * @return mixed The final value after each item has been reduced over.
+     */
+    function iter_reduce(?iterable $iterable, callable $reducer, mixed $initialValue = null): mixed
+    {
+        foreach ($iterable ?? [] as $key => $value) {
+            $initialValue = $reducer($initialValue, $value, $key);
+        }
+
+        return $initialValue;
     }
 }
--- tests/IdentityTest.php
+++ tests/IdentityTest.php
@@ -2,7 +2,9 @@
 namespace IterTools\Tests;
 
 use PHPUnit\Framework\TestCase;
-use function IterTools\all;
+use function IterTools\iter_all;
+use function IterTools\iter_count;
+use function IterTools\iter_reduce;
 
 final class IdentityTest extends TestCase
 {
@@ -15,6 +17,19 @@ final class IdentityTest extends TestCase
     /** Tests to make sure that function imports work; and that the all() function is working as intended. */
     public function testAll()
     {
-        $this->assertEquals([1, 2, 3], \IterTools\all([1, 2, 3]));
+        $this->assertEquals([1, 2, 3], \IterTools\iter_all([1, 2, 3]));
+    }
+
+    /** Tests to see if this counts arrays properly */
+    public function testCount()
+    {
+        $this->assertEquals(4, iter_count([1, 2, 3, 4]));
+    }
+
+    /** Tests to see if we can reduce arrays or not. */
+    public function testReduce()
+    {
+        $this->assertEquals(10, iter_reduce([1, 2, 3, 4], fn ($col, $it) => $col + $it), 0);
+        // TODO: Test which uses a key from the array
     }
 }
