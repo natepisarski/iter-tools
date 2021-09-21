42596d340264d5433138e32a8c57a7e81c15b945
Nathaniel Pisarski <nathanpisarski@gmail.com>
2021-09-20 20:14:52 -0400

    Initial Commit - Get a basic composer template set up

---
+++ tests/IdentityTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace IterTools\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+final class IdentityTest extends TestCase
+{
+    /** Tests to make sure that composer and PHPUnit are properly working. */
+    public function testAssertThatTestsRunInTheFirstPlace()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
